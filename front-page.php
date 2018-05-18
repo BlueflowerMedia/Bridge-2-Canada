024afb4e881a5407e7e8a1d96dc2a388fc21b045
Altab Hossain <altab_ru@yahoo.com>
2018-05-18 16:00:38 -0400

    banner added.

--- front-page.php
+++ front-page.php
@@ -1,9 +1,10 @@
 <?php get_header(); ?>
 
-<h1>test message</h1>
-
 <section class="banner">
-  
+  <div class="right-top">
+    <p><strong>Coming to Canada <br> made simple.</strong></p>  
+  </div>
+
 </section>
 
 <?php get_footer(); ?>
